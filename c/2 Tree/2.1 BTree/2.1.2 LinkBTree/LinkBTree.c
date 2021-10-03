1cb4934e75227c76e66282827a494d9e424cf89b
D_bxg <ganjinzheng@163.com>
2021-10-03 15:17:14 +0800

    递归和非递归遍历

--- c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.c
+++ c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.c
@@ -2,7 +2,7 @@
  * @Author: D_bxg
  * @Date: 2021-09-28 09:43:46
  * @LastEditors: D_bxg
- * @LastEditTime: 2021-10-02 10:52:33
+ * @LastEditTime: 2021-10-03 11:17:37
  * @Description: file content
  * @FilePath: \Ce:\Code\Data-Structures-and-Algorithms\data-structures-and-algorithms\c\2 Tree\2.1 BTree\2.1.2 LinkBTree\LinkBTree.c
  */
@@ -19,4 +19,56 @@ pNode initBTree(){
     pHead->pLChile = NULL;
     pHead->pRChile = NULL;
     return pHead;
+}
+void preOrderTraverse(pNode pHead){
+    if(pHead != NULL){
+        printf("%d",pHead->data);
+        preOrderTraverse(pHead->pLChile);
+        preOrderTraverse(pHead->pRChile);
+    }else{
+        return;
+    }
+}
+void inOrderTraverse(pNode pHead)
+{
+    if (pHead != NULL)
+    {
+        preOrderTraverse(pHead->pLChile);
+        printf("%d", pHead->data);
+        preOrderTraverse(pHead->pRChile);
+    }
+    else
+    {
+        return;
+    }
+}
+void postOrderTraverse(pNode pHead){
+    if (pHead != NULL)
+    {
+        preOrderTraverse(pHead->pLChile);
+        preOrderTraverse(pHead->pRChile);
+        printf("%d", pHead->data);
+    }
+    else
+    {
+        return;
+    }
+}
+// 借助栈
+void inOrderTraverse(pNode pHead){
+    pNode pTemp,pVal;
+    pLStack pS;
+    pS = initLStack();
+    pTemp = pHead;
+    while(pTemp || !isEmpty(pS)){
+        if(pTemp){
+            pushElem(pS, pTemp);
+            pTemp = pTemp->pLChile;
+        }else{
+            popElem(pS, pVal);
+            printf("%d",pVal->data);
+            p = pVal->pRChile;
+        }
+    }
+    return;
 }
--- c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.h
+++ c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.h
@@ -2,7 +2,7 @@
  * @Author: D_bxg
  * @Date: 2021-09-28 09:41:52
  * @LastEditors: D_bxg
- * @LastEditTime: 2021-10-02 10:55:10
+ * @LastEditTime: 2021-10-03 11:21:15
  * @Description: file content
  * @FilePath: \Ce:\Code\Data-Structures-and-Algorithms\data-structures-and-algorithms\c\2 Tree\2.1 BTree\2.1.2 LinkBTree\LinkBTree.h
  */
@@ -24,3 +24,11 @@ typedef struct Node{
 } Node, *pNode;
 
 pNode initBTree();
+// 递归方式
+void preOrderTraverse(pNode pHead);
+void inOrderTraverse(pNode pHead);
+void postOrderTraverse(pNode pHead);
+// 非递归，借助栈实现
+void inOrderTraverse(pNode pHead);
+// 层次遍历，逐层遍历，借助队列Queue
+void levelOrder();
