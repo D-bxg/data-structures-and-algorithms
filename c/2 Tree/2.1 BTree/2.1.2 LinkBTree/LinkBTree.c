38d48b8c32b8b01c921d6f60c7ca79f8ecb3e5fe
D_bxg <ganjinzheng@163.com>
2021-10-06 14:20:31 +0800

    树的一些算法更新完毕

--- c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.c
+++ c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.c
@@ -2,7 +2,7 @@
  * @Author: D_bxg
  * @Date: 2021-09-28 09:43:46
  * @LastEditors: D_bxg
- * @LastEditTime: 2021-10-03 11:17:37
+ * @LastEditTime: 2021-10-06 11:09:08
  * @Description: file content
  * @FilePath: \Ce:\Code\Data-Structures-and-Algorithms\data-structures-and-algorithms\c\2 Tree\2.1 BTree\2.1.2 LinkBTree\LinkBTree.c
  */
@@ -71,4 +71,90 @@ void inOrderTraverse(pNode pHead){
         }
     }
     return;
+}
+void levelOrder(pNode pHead){
+    pNode pTemp;
+    pLQueue pQ;
+    pQ = initLQueue();
+    enLQueue(pQ,pHead); // 把二叉树根结点入队
+    while(!isEmpty(pQ)){
+        deLQueue(pQ, pTemp); // 出队
+        printf("%d", pTemp->data);
+        if(pTemp->pLChile != NULL){
+            enLQueue(pQ, pTemp->pLChile);
+        }
+        if (pTemp->pRChile != NULL){
+            enLQueue(pQ, pTemp->pRChile);
+        }
+    }
+    return;
+}
+/**
+ * @description: 按照用户输入的字符串先序生成一颗树,ABC##DE#G##F##
+ * @param {pNode} pHead
+ * @return {*}
+ */
+void preCreateBTree(pNode pHead){
+    char temp;
+    scanf("%c", &temp);
+    if(temp == '#'){
+        pHead = NULL;
+    }else{
+        if(!(pHead = (pNode)malloc(sizeof(Node)))){
+            exit(-1);
+        }else{
+            pHead->data = temp; // 此处由于我定义的Node的data是int类型，并不能正常运行，把data改成char就好了。
+            preCreateBTree(pHead->pLChile);
+            preCreateBTree(pHead->pRChile);
+        }
+    }
+    return;
+}
+void preCopyBTree(pNode pHead, pNode pNew){
+    if(pHead == NULL){
+        pNew == NULL;
+        return;
+    }else{
+        pNew = (pNode)malloc(sizeof(Node));
+        if(pNew == NULL){
+            exit(-1)
+        }else{
+            pNew->data = pHead->data;
+            preCopyBTree(pHead->pLChile, pNew->pLChile);
+            preCopyBTree(pHead->pRChile, pNew->pRChile);
+        }
+    }
+    return;
+}
+int depth(pNode pHead){
+    if(pHead == NULL){
+        retrun 0;
+    }else{
+        int m , n;
+        m = depth(pHead->pLChile);
+        n = depth(pHead->pRChile);
+        if(m > n){
+            return m + 1;
+        }else{
+            return n + 1;
+        }
+    }
+}
+int nodeCount(pNode pHead){
+    if(pHead == NULL){
+        return 0;
+    }else{
+        return nodeCount(pHead->pLChile) + nodeCount(pHead->pRChile) + 1;
+    }
+}
+int leafCount(pNode pHead){
+    if(pHead == NULL){
+        return 0;
+    }else{
+        if(pHead->pLChile == NULL && pHead->pRChile == NULL){
+            return 1;
+        }else{
+            return leafCount(pHead->pLChile) + leafCount(pHead->pRChile);
+        }
+    }
 }
--- c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.h
+++ c/2 Tree/2.1 BTree/2.1.2 LinkBTree/LinkBTree.h
@@ -2,7 +2,7 @@
  * @Author: D_bxg
  * @Date: 2021-09-28 09:41:52
  * @LastEditors: D_bxg
- * @LastEditTime: 2021-10-03 11:21:15
+ * @LastEditTime: 2021-10-06 11:07:10
  * @Description: file content
  * @FilePath: \Ce:\Code\Data-Structures-and-Algorithms\data-structures-and-algorithms\c\2 Tree\2.1 BTree\2.1.2 LinkBTree\LinkBTree.h
  */
@@ -12,7 +12,7 @@
 
 typedef int ElemType;
 // typedef struct ElemType{
-//     int elem;
+//     Node data;
 // };
 
 typedef struct Node{
@@ -29,6 +29,23 @@ void preOrderTraverse(pNode pHead);
 void inOrderTraverse(pNode pHead);
 void postOrderTraverse(pNode pHead);
 // 非递归，借助栈实现
+/**
+ * typedef struct StackNode{
+ *      ElemType data; // 这里的ElemType就是此处的Node结构体。也就是说我们可以使用StackNode.data.data来调用到二叉树的结点值。
+ *      struct StackNode * pNext;
+ * } StackNode, *pStackNode;
+*/
 void inOrderTraverse(pNode pHead);
 // 层次遍历，逐层遍历，借助队列Queue
-void levelOrder();
+/**
+ * typedef struct QueueNode{
+ *      ElemType data; // QueueNode.data.data.data。每层结构体分别为：QueueNode，ElemType，Node
+ *      struct QueueNode * front,rear;
+ * } QueueNode, *pQueueNode
+*/
+void levelOrder(pNode pHead);
+void preCreateBTree(pNode pHead);
+void preCopyBTree(pNode pHead, pNode pNew);
+int depth(pNode pHead);
+int nodeCount(pNode pHead);
+int leafCount(pNode pHead);
